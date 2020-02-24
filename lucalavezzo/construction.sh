f8fbfd0b390b1d3cb4c7fef14d6987deeadf56e9
lucalavezzo <38077849+lucalavezzo@users.noreply.github.com>
2020-02-24 14:28:50 -0500

    construction simulation

---
+++ lucalavezzo/construction.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source /users/PAS1585/llavez99/work/rl_flow/setup_sumo.sh
+python -u /users/PAS1585/llavez99/work/rl_flow/rl_training_construction.py >& /users/PAS1585/llavez99/work/rl_flow/training_construction.lg
